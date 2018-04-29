4467f29d6d952b6b7ba4f2990e9126b538af14c6
Alvaro <alvarojnavarro@hotmail.com>
2018-04-29 21:38:29 +0200

    Contact form added
    Success

--- contact.php
+++ contact.php
@@ -4,18 +4,24 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = email;
+$from = 'email@detexian.com';
+
 // an email address that will receive the email with the output of the form
 $sendTo = 'nralvaro@gmail.com';
+
 // subject of the email
 $subject = 'New message from contact form';
+
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
+$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); 
+
 // message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+$okMessage = 'Thank you! We have received your message and will get back to you as soon as possible.';
+
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
+
 /*
  *  LET'S DO THE SENDING
  */
